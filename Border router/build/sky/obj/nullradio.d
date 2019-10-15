fcc4214f43c876edda0b811ecacd1bd1642fda91
filippopiconese <filippopiconese@hotmail.it>
2019-10-15 10:03:56 +0200

    adjusted .gitignore

--- Border router/build/sky/obj/nullradio.d
+++
@@ -1,8 +0,0 @@
-build/sky/obj/nullradio.o: ../../../os/dev/nullradio.c \
- sky/module-macros.h \
- ../../../os/services/rpl-border-router/module-macros.h \
- ../../../os/dev/nullradio.h ../../../os/dev/radio.h
-../../../os/dev/nullradio.c :
- sky/module-macros.h :
- ../../../os/services/rpl-border-router/module-macros.h :
- ../../../os/dev/nullradio.h ../../../os/dev/radio.h :
